a1f7147c61e0f928b9e79e164c5801cab247979e
jkasprowicz <joao.kasprowicz@univali.br>
2024-11-12 10:41:37 -0300

    Initial Commit

---
+++ stressTests/stressTests.swift
@@ -0,0 +1,17 @@
+//
+//  stressTests.swift
+//  stressTests
+//
+//  Created by João Kasprowicz on 12/11/24.
+//
+
+import Testing
+@testable import stress
+
+struct stressTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
